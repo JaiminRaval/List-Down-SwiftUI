c581ccaf510e288736db5d61bde995443e4dc0ad
JaiminRaval <siliconvalley.jaimin@gmail.com>
2026-07-07 23:11:52 +0530

    added initial Implement of List with navigation Stack

--- List Down/ContentView.swift
+++ List Down/ContentView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fruitsArr: [String] = [
+        "Apples",
+        "Oranges",
+        "Coconut",
+        "Raspberry",
+        "Pomegranate",
+        "Banana",
+        "Coldwater",
+        "Blueberry",
+        "",
+        "",
+        "",
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(fruitsArr, id: \.self) { fruit in
+                    Text(fruit)
+                    
+                }
+                .onDelete(perform: swipeToDelete)
+            }
+            .navigationTitle("Fruits")
+
         }
-        .padding()
+    }
+    private func swipeToDelete(at offset: IndexSet) {
+        fruitsArr.remove(atOffsets: offset)
+        print(fruitsArr)
     }
 }
+
+
 
 #Preview {
     ContentView()
